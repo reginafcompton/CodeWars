70a51b5ce61d632c9d6b39b4c2a93862d2e78c95
reginafcompton <reginafcompton@gmail.com>
2016-02-02 09:33:17 -0600

    Add solution for dont_drink_water

---
+++ dont_drink_water.rb
@@ -0,0 +1,26 @@
+def separate_liquids(glass)
+  empty_glass = []
+  return empty_glass if glass.empty? == true
+
+  amounts_hash = make_hash(glass)
+  amounts_hash.each do |key, value|
+    value.times { empty_glass << key }
+  end
+
+  glass_width = glass[0].length
+  empty_glass.each_slice(glass_width).to_a
+end
+
+def make_hash(glass)
+  liquid_amounts = { "O" => 0, "A" => 0, "W" => 0, "H" => 0 }
+  glass_flat_array = glass.flatten
+  glass_flat_array.each do |el|
+    liquid_amounts[el] += 1
+  end
+  liquid_amounts
+end
+
+
+p separate_liquids([['H', 'H', 'W', 'O'], ['H', 'H', 'W', 'O']])
+p separate_liquids([])
+
---
+++ wally.rb
@@ -0,0 +1,13 @@
+def wheres_wally(string)
+   if /Wally[[:alnum:]]/.match(string) || /[[:alnum:]]Wally/.match(string) || /[[:punct:]]Wally/.match(string)
+      -1
+   elsif /Wally[[:punct:]]/.match(string) || /Wally/.match(string)
+      string.index("Wally")
+   else
+      -1
+   end
+end
+
+
+p wheres_wally("I am ffWally,")
+
